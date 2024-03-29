3cabc20832e55042452164cfaffcb46e8dece8f1
Christopher Schütz <Christopher.Schuetz@pm.me>
2024-03-29 08:33:54 +0100

    Refine keymap

--- keyboards/voyager/keymaps/chris-schuetz/keymap.c
+++ keyboards/voyager/keymaps/chris-schuetz/keymap.c
@@ -12,10 +12,10 @@ enum custom_keycodes {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT_voyager(
     DE_LBRC,        KC_1,           KC_2,           KC_3,           KC_4,           KC_5,                                           KC_6,           KC_7,           KC_8,           KC_9,           KC_0,           DE_RBRC,
-    KC_TAB,         KC_B,           DE_Y,           KC_O,           KC_U,           DE_QUOT,                                        DE_DQOT,        KC_L,           KC_D,           KC_W,           KC_V,           DE_Z,
-    KC_LEFT_ALT,    KC_C,           KC_I,           KC_E,           KC_A,           KC_COMMA,                                       KC_DOT,         KC_H,           KC_T,           KC_S,           KC_N,           KC_Q,
+    KC_LEFT_ALT,    KC_B,           DE_Y,           KC_O,           KC_U,           DE_QUOT,                                        DE_DQOT,        KC_L,           KC_D,           KC_W,           KC_V,           DE_Z,
+    KC_LEFT_CTRL,   KC_C,           KC_I,           KC_E,           KC_A,           KC_COMMA,                                       KC_DOT,         KC_H,           KC_T,           KC_S,           KC_N,           KC_Q,
     KC_LEFT_GUI,    KC_G,           KC_X,           KC_J,           KC_K,           DE_MINS,                                        DE_EXLM,        KC_R,           KC_M,           KC_F,           KC_P,           KC_BSPC,
-                                                                                    LT(1,KC_SPACE), KC_LEFT_CTRL,   KC_RIGHT_SHIFT, LT(4,KC_ENTER)
+                                                                                    LT(1,KC_SPACE), KC_TAB,         KC_RIGHT_SHIFT, LT(4,KC_ENTER)
   ),
   [1] = LAYOUT_voyager(
     KC_ESCAPE,      KC_F1,          KC_F2,          KC_F3,          KC_F4,          KC_F5,                                          KC_F6,          KC_F7,          KC_F8,          KC_F9,          KC_F10,         KC_F11,
@@ -42,7 +42,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     RGB_TOG,        TOGGLE_LAYER_COLOR,RGB_MODE_FORWARD,RGB_SLD,        RGB_VAD,        RGB_VAI,                                        KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, QK_BOOT,
     KC_TRANSPARENT, KC_TRANSPARENT, KC_AUDIO_VOL_DOWN,KC_AUDIO_VOL_UP,KC_AUDIO_MUTE,  KC_TRANSPARENT,                                 KC_TRANSPARENT, KC_TRANSPARENT, KC_PAGE_UP,     KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
     KC_TRANSPARENT, KC_MEDIA_PREV_TRACK,KC_MEDIA_NEXT_TRACK,KC_MEDIA_STOP,  KC_MEDIA_PLAY_PAUSE,KC_TRANSPARENT,                                 KC_TRANSPARENT, KC_HOME,        KC_PGDN,        KC_END,         KC_TRANSPARENT, KC_TRANSPARENT,
-    TO(0),          TO(2),          TO(3),          KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,                                 KC_TRANSPARENT, LCTL(LSFT(KC_TAB)),LCTL(KC_TAB),   KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
+    KC_TRANSPARENT, TO(0),          TO(2),          TO(3),          KC_TRANSPARENT, KC_TRANSPARENT,                                 KC_TRANSPARENT, LCTL(LSFT(KC_TAB)),LCTL(KC_TAB),   KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
                                                                                     KC_LEFT_ALT,    KC_LEFT_CTRL,   KC_TRANSPARENT, KC_TRANSPARENT
   ),
 };
@@ -50,7 +50,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 const key_override_t delete_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_BSPC, KC_DEL);
 const key_override_t lcbr_key_override = ko_make_basic(MOD_MASK_SHIFT, DE_LBRC, DE_LCBR);
 const key_override_t pipe_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_1, DE_PIPE);
-const key_override_t equal_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_2, DE_EQL);
+const key_override_t equl_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_2, DE_EQL);
 const key_override_t tild_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_3, DE_TILD);
 const key_override_t plus_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_4, DE_PLUS);
 const key_override_t less_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_5, DE_LESS);
@@ -62,7 +62,7 @@ const key_override_t astr_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_0, DE_
 const key_override_t rcbr_key_override = ko_make_basic(MOD_MASK_SHIFT, DE_RBRC, DE_RCBR);
 const key_override_t lprn_key_override = ko_make_basic(MOD_MASK_SHIFT, DE_QUOT, DE_LPRN);
 const key_override_t rprn_key_override = ko_make_basic(MOD_MASK_SHIFT, DE_DQOT, DE_RPRN);
-const key_override_t qst_key_override = ko_make_basic(MOD_MASK_SHIFT, DE_EXLM, DE_QST);
+const key_override_t qust_key_override = ko_make_basic(MOD_MASK_SHIFT, DE_EXLM, DE_QST);
 const key_override_t iexl_key_override = ko_make_basic(MOD_MASK_SHIFT, ES_IEXL, ES_IQUE);
 
 // This globally defines all key overrides to be used
@@ -70,7 +70,7 @@ const key_override_t **key_overrides = (const key_override_t *[]){
 	&delete_key_override,
     &lcbr_key_override,
     &pipe_key_override,
-    &equal_key_override,
+    &equl_key_override,
     &tild_key_override,
     &plus_key_override,
     &less_key_override,
@@ -82,7 +82,7 @@ const key_override_t **key_overrides = (const key_override_t *[]){
     &rcbr_key_override,
     &lprn_key_override,
     &rprn_key_override,
-    &qst_key_override,
+    &qust_key_override,
     &iexl_key_override,
 	NULL // Null terminate the array of overrides!
 };
